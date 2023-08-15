d8f00af69334f29141b3e58047d3f6deaf016dd7
mssssss123 <824186479@qq.com>
2023-08-15 10:40:20 +0800

    First Commit

---
+++ reproduce/train/toys/train_toys.sh
@@ -0,0 +1,20 @@
+ export CUDA_VISIBLE_DEVICES=3
+ nohup python train.py  \
+    --output_dir /data1/meisen/TASTE-main/checkpoint/toys/name  \
+    --model_name_or_path /data1/meisen/pretrained_model/t5-base  \
+    --do_train  \
+    --save_steps 5000  \
+    --eval_steps 5000  \
+    --train_path /data1/meisen/TASTE-main/Data/toys/train_name.jsonl  \
+    --eval_path /data1/meisen/TASTE-main/Data/toys/valid_name.jsonl  \
+    --per_device_train_batch_size 8  \
+    --per_device_eval_batch_size 8  \
+    --train_n_passages 10  \
+    --num_passages 2  \
+    --learning_rate 1e-4  \
+    --q_max_len 256  \
+    --p_max_len 32  \
+    --seed 2022  \
+    --num_train_epochs 30  \
+    --evaluation_strategy steps  \
+    --logging_dir /data1/meisen/TASTE-main/checkpoint/toys/name-log    > train_toys.out  2>&1 &   
